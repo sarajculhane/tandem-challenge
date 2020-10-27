a496022ce30ec4748d30b760063e8ed445a0db48
Sara Culhane <sjcully@mac.com>
2020-10-27 17:36:55 -0400

    initial set up for app

---
+++ client/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>Hello world lol</div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}; 
