78423ca6df8828a7664a239d071aa065b42d1757
Sara Culhane <sjcully@mac.com>
2020-10-31 14:51:43 -0400

    replay button

--- client/components/QuestionList.js
+++ client/components/QuestionList.js
@@ -1,37 +1,43 @@
 import React, {useState} from 'react'
-import QuestionCard from './QuestionCard'
-// import QuestionCard from './QuestionCard'
 
 const QuestionList = (props) => {
     const {questions} = props
     const [currentIdx, setIdx] = useState(1)
     const [currentQuestion, setQuestion] = useState(questions[0])
     console.log(questions, currentIdx, currentQuestion)
+    
+    const reset = () => {
+        setIdx(1)
+        setQuestion(questions[0])
+    }
+
     const handleClick = () => {
         event.preventDefault()
         
-        if(questions[currentIdx]  && currentIdx <= questions.length) {
-            
-            console.log(currentIdx)
+        if(questions[currentIdx]  && currentIdx < questions.length) {
             setQuestion(questions[currentIdx])
             setIdx(currentIdx + 1)
             
             
         }
-        if(currentIdx > questions.length) {
-            // return (
-            //     <div>Game Over</div>
-            // )
-            console.log('game over')
+        else {
+            setIdx(questions.length + 1)
+            // console.log('game over')
         }
     }
 
     return (
         
         <div>
+            {currentIdx === questions.length + 1 ?
+        <div>
+            <PlayAgain replay={reset} />
+        </div>
+          :
+
         <div className="card" style={{width: 18 + 'rem'}}>
           <div className="card-body">
-            <h5 className="card-title">Question {currentIdx+ 1}</h5>
+            <h5 className="card-title">Question {currentIdx}</h5>
             <p className="card-text" color='black'>{currentQuestion.question}</p>
           </div>
           <ul className="list-group list-group-flush">
@@ -41,14 +47,30 @@ const QuestionList = (props) => {
           </ul>
           <div className="card-body">
             <form>
-              <button type="submit" onClick={handleClick}>Next</button>
+            <button type="submit" className="btn btn-primary" onClick={reset}>Reset</button>
+              <button type="submit" className="btn btn-primary" onClick={handleClick}>Next</button>
             </form>
           </div>
         </div> 
+}
         </div>
     )
         
 
+}
+
+export const PlayAgain = (props) => {
+    const {replay} = props
+    return (
+        <div className="card" style={{width: 18 + 'rem'}}>
+        <div className="card-body">
+            <h5 className="card-title">Do you want to play again?</h5>
+            <p className="card-text" color='black'>
+                <button type="submit" className="btn btn-primary" onClick={replay}>Play Again</button>
+            </p>
+</div>
+</div>
+    )
 }
 
 
