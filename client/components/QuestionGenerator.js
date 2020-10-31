d9fa172987c79156b9a41062fcc80ccbcd1d2d62
Sara Culhane <sjcully@mac.com>
2020-10-31 18:32:55 -0400

    can add to score

--- client/components/QuestionGenerator.js
+++ client/components/QuestionGenerator.js
@@ -13,12 +13,22 @@ const QuestionGenerator = () => {
         while(i < questions.length)  {
             let idx = Math.floor(Math.random()*questions.length)
             questions[i] = vals[idx]
-            vals.splice(idx, 1)
+            vals.slice(idx, 1)[0]
             i++
         }
         return questions
     }
+
+    // used to create a new property on each question with a random ordering of answers
+    // the correct answer is still an object so that we can check for this later
+    const getChoices = (question) => {
+        const choices = [{correct: question.correct}, ...question.incorrect]
+        const shuffle = (choices) => choices.sort(() => Math.random()- 0.5)
+        return shuffle(choices)
+    }
+
     const questions = randomSelect()
+    questions.forEach((question) => question.choices = getChoices(question))
 
     return (
         
--- client/components/QuestionList.js
+++ client/components/QuestionList.js
@@ -1,43 +1,42 @@
 import React, {useState} from 'react'
 
 const QuestionList = (props) => {
-    const {questions} = props
+    let {questions} = props
     const [currentIdx, setIdx] = useState(1)
     const [currentQuestion, setQuestion] = useState(questions[0])
-    const [selectedAnswer, setSelected] = useState('')
+    const [selected, setSelected] = useState(false)
+    const [target, setTarget] = useState(0)
+
     const [score, setScore] = useState(0)
 
-    const getChoices = (question) => {
-        const choices = [{correct: question.correct}, ...question.incorrect]
-        console.log(choices)
-        const shuffle = (choices) => choices.sort(() => Math.random()- 0.5)
-        return shuffle(choices)
-    }
+
     const reset = () => {
         setIdx(1)
         setQuestion(questions[0])
     }
 
     const handleSelect = () => {
-        console.log(event.target.value)
-        setSelected(event.target.value)
+        // event.preventDefault()
+        console.log(event.target.value, 'the target val')
+        setSelected(true)
+        setTarget(event.target.value)
+        // setSelected(event.target.value)
     }
 
     const handleClick = () => {
         event.preventDefault()
-
+        console.log(currentQuestion.choices[target])
+        if(typeof currentQuestion.choices[target] === 'object') setScore(prev => prev + 1)
         if(questions[currentIdx]  && currentIdx < questions.length) {
             setQuestion(questions[currentIdx])
             setIdx(currentIdx + 1)
-            
+            setSelected(false)
         }
         else {
             setIdx(questions.length + 1)
             // console.log('game over')
         }
     }
-
-    const choices = getChoices(currentQuestion)
     return (
         
         <div>
@@ -57,14 +56,14 @@ const QuestionList = (props) => {
           <div className="card-body">
             <form>
             <ul className="list-group list-group-flush">
-                {choices.map((choice) => {
+                {currentQuestion.choices.map((choice, idx) => {
+    
                     return(
                         <div>
                     
                     
                     <li className="list-group-item">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" onChange={handleSelect}
-                    value={choice}></input>
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" onChange={handleSelect} value={idx}></input>
                         {typeof choice === 'string' ? choice : choice.correct}</li>
                     </div>
                     
@@ -74,8 +73,8 @@ const QuestionList = (props) => {
 
           </ul>
             <button type="submit" className="btn btn-primary" onClick={reset}>Reset</button>
-             { selectedAnswer.length ?  <button type="submit" className="btn btn-primary" onClick={handleClick}>Next</button> :
-                <button type="submit" className="btn btn-primary" onClick={handleClick} disabled>Next</button>}
+            {selected ? <button type="submit" className="btn btn-primary" onClick={handleClick}>Next</button>  :
+            <button type="submit" className="btn btn-primary" onClick={handleClick} disabled>Next</button> }
             </form>
           </div>
         </div> 
