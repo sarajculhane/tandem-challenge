f7833343fa6054415e89be2dd0b733a84b37b0e9
Sara Culhane <sjcully@mac.com>
2020-11-01 08:33:08 -0500

    added App.js tests

---
+++ client/components/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+ 
+Enzyme.configure({ adapter: new Adapter() });
+import App from "./App";
+import QuestionGenerator from './QuestionGenerator'
+
+
+it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it('renders the QuestionGenerator componenent', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('QuestionGenerator').length === 1).toBe(true)
+});
